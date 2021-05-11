6afe0886c0aec042e7eb64d8a3927e70a10280f2
yavord10 <yavor.v.dimitrov@gmail.com>
2021-05-11 17:02:31 +0800

    more changes to tutorial app

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,9 +1,45 @@
 import { Template } from 'meteor/templating';
 import { TasksCollection } from "../api/TasksCollection";
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './App.html';
+import './Task.js';
+
+const HIDE_COMPLETED_STRING = "hideCompleted";
+
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
+});
 
 Template.mainContainer.helpers({
   tasks() {
-    return TasksCollection.find({});
+    return TasksCollection.find({}, { sort: { createdAt: -1 } });
   },
+});
+
+Template.mainContainer.events({
+  "click #hide-completed-button"(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  }
+})
+
+Template.form.events({
+  "submit .task-form"(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    // Insert a task into the collection
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+
+    // Clear form
+    target.text.value = '';
+  }
 });
